79242210f111e14267f8845df072ad76e794f2ae
Stian Sanden Skauge <stian@skauge.org>
2024-08-29 22:14:33 +0200

    ♻️ Refactored a bit

--- main.go
+++ main.go
@@ -13,107 +13,26 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
-var debug bool
+var (
+	debug       bool
+	fileOffsets = make(map[string]int64)
+	mu          sync.Mutex
+)
 
-func watchDir(dir string) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	var mu sync.Mutex
-	fileOffsets := make(map[string]int64)
-
-	go handleEvents(watcher, &mu, fileOffsets)
-
-	addWatcherForRootDir(watcher, dir)
-	walkAndWatchFiles(watcher, dir, &mu, fileOffsets)
-
-	<-make(chan struct{})
+func newOffset(file *os.File) int64 {
+	newOffset, _ := file.Seek(0, os.SEEK_CUR)
+	return newOffset
 }
 
-func handleEvents(watcher *fsnotify.Watcher, mu *sync.Mutex, fileOffsets map[string]int64) {
-	for {
-		select {
-		case event, ok := <-watcher.Events:
-			if !ok {
-				return
-			}
-			if debug {
-				log.Printf("[debug] event: %s", event)
-			}
-			if event.Op&fsnotify.Create == fsnotify.Create {
-				handleCreateEvent(watcher, event, mu, fileOffsets)
-			}
-			if event.Op&fsnotify.Write == fsnotify.Write {
-				handleWriteEvent(event, mu, fileOffsets)
-			}
-		case err, ok := <-watcher.Errors:
-			if !ok {
-				return
-			}
-			log.Println("error:", err)
-		}
-	}
-}
-
-func handleCreateEvent(watcher *fsnotify.Watcher, event fsnotify.Event, mu *sync.Mutex, fileOffsets map[string]int64) {
-	info, err := os.Stat(event.Name)
-	if err == nil && !info.IsDir() && !strings.HasPrefix(info.Name(), ".") {
-		mu.Lock()
-		fileOffsets[event.Name] = readNewLines(event.Name, 0)
-		watcher.Add(event.Name)
-		mu.Unlock()
-	}
-}
-
-func handleWriteEvent(event fsnotify.Event, mu *sync.Mutex, fileOffsets map[string]int64) {
+func processFile(filePath string) {
 	mu.Lock()
-	offset := fileOffsets[event.Name]
-	fileOffsets[event.Name] = readNewLines(event.Name, offset)
+	offset := fileOffsets[filePath]
 	mu.Unlock()
-}
 
-func addWatcherForRootDir(watcher *fsnotify.Watcher, dir string) {
-	err := watcher.Add(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func walkAndWatchFiles(watcher *fsnotify.Watcher, dir string, mu *sync.Mutex, fileOffsets map[string]int64) {
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() && path != dir {
-			return filepath.SkipDir
-		}
-		if !info.IsDir() && strings.HasPrefix(info.Name(), ".") {
-			return nil
-		}
-		if !info.IsDir() {
-			err = watcher.Add(path)
-			if err != nil {
-				return err
-			}
-			mu.Lock()
-			fileOffsets[path] = readNewLines(path, 0)
-			mu.Unlock()
-		}
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func readNewLines(filePath string, offset int64) int64 {
 	file, err := os.Open(filePath)
 	if err != nil {
-		log.Println("error opening:", err)
-		return offset
+		log.Println("error:", err)
+		return
 	}
 	defer file.Close()
 
@@ -129,8 +48,76 @@ func readNewLines(filePath string, offset int64) int64 {
 		log.Println("error reading file:", err)
 	}
 
-	newOffset, _ := file.Seek(0, os.SEEK_CUR)
-	return newOffset
+	updateOffset(filePath, newOffset(file))
+}
+
+func updateOffset(filePath string, offset int64) {
+	mu.Lock()
+	defer mu.Unlock()
+	fileOffsets[filePath] = offset
+}
+
+func handleFileEvent(event fsnotify.Event) {
+	if event.Op&fsnotify.Create == fsnotify.Create || event.Op&fsnotify.Write == fsnotify.Write {
+		if debug {
+			log.Println(event.Op, event.Name)
+		}
+		processFile(event.Name)
+	}
+}
+
+func watchDir(dir string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				handleFileEvent(event)
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	if err = watcher.Add(dir); err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
+
+func ensureDirExists(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			log.Fatalf("Failed to create directory: %s", dir)
+		}
+	}
+}
+
+func processInitialFiles(dir string) {
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			processFile(path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Failed to process initial files: %s", err)
+	}
 }
 
 func main() {
@@ -143,6 +130,7 @@ func main() {
 	}
 
 	*dir = filepath.Clean(*dir) + string(os.PathSeparator)
-
+	ensureDirExists(*dir)
+	processInitialFiles(*dir)
 	watchDir(*dir)
 }
